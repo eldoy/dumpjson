48dd57439ac988372ca6b5694d8e906d25d9d795
Vidar Eldøy <vidar@eldoy.com>
2024-11-06 12:32:19 +0100

    fix: remove unused

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function (name, data) {
+module.exports = function (name) {
   if (/^\w/.test(name)) {
     name = path.join(process.cwd(), name)
   }
